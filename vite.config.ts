fe53dd7770f30d0743ae461184a537eb447f43b7
kimcoder <kimcoder@me.com>
2025-07-22 22:06:45 +0900

    chore: remove unused param

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import manifest from './src/manifest';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     build: {
       emptyOutDir: true,
